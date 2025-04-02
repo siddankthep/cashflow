5fca341873277d4783fc30425a355ab05a85bd85
Sid <quan.nguyen.210257@student.fulbright.edu.vn>
2025-04-02 09:50:48 +0700

    remove env from build

--- frontend/cashflow/lib/main.dart
+++ frontend/cashflow/lib/main.dart
@@ -3,13 +3,16 @@ import 'package:cashflow/view/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cashflow/model/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:io' show Platform;
 
 import 'package:camera/camera.dart';
 
 Future<void> main() async {
   // Load the .env file
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
+  String? apiUrl = Platform.environment['API_URL'];
+  print('API URL: $apiUrl');
 
   // Existing initialization code
   WidgetsFlutterBinding.ensureInitialized();
