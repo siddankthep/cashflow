e7a5e15140006cfca30d6c299e5a48ba595708de
Sid <quan.nguyen.210257@student.fulbright.edu.vn>
2025-04-30 18:05:22 +0700

    remove user service

--- backend/cashflow/src/main/java/com/example/cashflow/authentication/controllers/UserController.java
+++ backend/cashflow/src/main/java/com/example/cashflow/authentication/controllers/UserController.java
@@ -7,17 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.cashflow.authentication.services.UserService;
 import com.example.cashflow.entities.User;
 
 @RequestMapping("/users")
 @RestController
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/me")
     public ResponseEntity<User> authenticatedUser() {
