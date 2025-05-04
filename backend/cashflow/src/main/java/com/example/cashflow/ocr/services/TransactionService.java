f40ea03fe2a36a1a79f1299ed9b37679a8e4d210
Sid <quan.nguyen.210257@student.fulbright.edu.vn>
2025-05-05 01:50:56 +0700

    update balance with new transaction

--- backend/cashflow/src/main/java/com/example/cashflow/ocr/services/TransactionService.java
+++ backend/cashflow/src/main/java/com/example/cashflow/ocr/services/TransactionService.java
@@ -6,8 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.example.cashflow.authentication.repositories.UserRepository;
 import com.example.cashflow.entities.Category;
 import com.example.cashflow.entities.Transaction;
 import com.example.cashflow.entities.User;
@@ -18,13 +21,16 @@ import com.example.cashflow.ocr.responses.CategoryResponse;
 
 @Service
 public class TransactionService {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
     private final TransactionRepository transactionRepository;
     private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
 
     public TransactionService(TransactionRepository transactionRepository,
-            CategoryRepository categoryRepository) {
+            CategoryRepository categoryRepository, UserRepository userRepository) {
         this.transactionRepository = transactionRepository;
         this.categoryRepository = categoryRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Transaction> getAllTransactions() {
@@ -57,6 +63,12 @@ public class TransactionService {
         if (input.getSubtotal().compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("Subtotal must be positive");
         }
+
+        BigDecimal newBalance = user.getBalance().subtract(input.getSubtotal());
+        user.setBalance(newBalance);
+        userRepository.save(user);
+        logger.info("New balance: " + newBalance);
+
         Transaction newTransaction = new Transaction(
                 user,
                 category,
