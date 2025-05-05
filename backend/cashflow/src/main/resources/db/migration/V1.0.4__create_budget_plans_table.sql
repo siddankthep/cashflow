477056afae3d693f91821f531821346bc01e5a91
Sid <quan.nguyen.210257@student.fulbright.edu.vn>
2025-05-05 20:49:44 +0700

    remove BE from branch

--- backend/cashflow/src/main/resources/db/migration/V1.0.4__create_budget_plans_table.sql
+++
@@ -1,7 +0,0 @@
--- V1.0.3__create_budget_plans_table.sql
-CREATE TABLE budget_plans (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id),
-    name VARCHAR(50),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
